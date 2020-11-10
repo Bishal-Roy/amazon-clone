bdbb758e0f4790dde575dcb8f101b07a0a3901f6
unknown <bishalr582@gmail.com>
2020-11-11 01:28:54 +0600

    first commit (header complite)

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD53dmoElOHzTfHalt_4bMyvX2-seL1n_I",
+  authDomain: "clone-2c57a.firebaseapp.com",
+  databaseURL: "https://clone-2c57a.firebaseio.com",
+  projectId: "clone-2c57a",
+  storageBucket: "clone-2c57a.appspot.com",
+  messagingSenderId: "349395969801",
+  appId: "1:349395969801:web:4b0df3c9533d20226615d8",
+  measurementId: "G-1N97JRSMTF",
+};
